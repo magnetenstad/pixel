df5676148345e8c724097b66cef18a589e5e6250
magnetenstad <tenstad.magne@gmail.com>
2021-02-16 23:40:47 +0100

    Center canvas

--- project/src/main/java/pixel/PixelController.java
+++ project/src/main/java/pixel/PixelController.java
@@ -3,17 +3,20 @@ package pixel;
 import java.util.List;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.control.Slider;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
@@ -70,9 +73,29 @@ public class PixelController {
 	private Tab addTab(String name) {
 		Tab tab = new Tab();
 		tab.setText(name);
-		tab.setContent(createCanvas());
-		
 		tabPane.getTabs().add(tab);
+		
+		ScrollPane scrollPane = new ScrollPane();
+		scrollPane.setOnScroll(event -> {
+			if (event.isControlDown()) {
+				double delta = event.getDeltaY() > 0 ? 0.1 : -0.1;
+				scrollPane.getContent().setScaleX(clamp(scrollPane.getContent().getScaleX() + delta, 0.1, 10));
+				scrollPane.getContent().setScaleY(clamp(scrollPane.getContent().getScaleY() + delta, 0.1, 10));
+			}
+	    });
+		scrollPane.setHbarPolicy(ScrollBarPolicy.ALWAYS);
+		scrollPane.setVbarPolicy(ScrollBarPolicy.ALWAYS);
+
+		tab.setContent(scrollPane);
+		
+		StackPane pane = new StackPane();
+		pane.setPrefHeight(1000);
+		pane.setPrefWidth(1000);
+		scrollPane.setContent(pane);
+		
+		Canvas canvas = createCanvas();
+		pane.getChildren().add(canvas);
+		StackPane.setAlignment(canvas, Pos.CENTER);
 		
 		return tab;
 	}
@@ -80,29 +103,37 @@ public class PixelController {
 	private Canvas createCanvas() {
 		Canvas canvas = new Canvas(600, 600);
 		GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-//		graphicsContext.setFill(Color.WHITE);
-//		graphicsContext.fill();
+		graphicsContext.setImageSmoothing(false);
+		graphicsContext.setFill(Color.WHITE);
+		graphicsContext.fillRect(0, 0, 600, 600);
 		
 		canvas.setOnMouseDragged(event -> {
 			switch (toolSelected) {
 				case "Pencil": 
 					graphicsContext.setFill(colorPicker.getValue());
-					graphicsContext.fillRect(event.getX(), event.getY(), toolSlider.getValue(), toolSlider.getValue());
+					graphicsContext.fillRect((int) event.getX(), (int) event.getY(), toolSlider.getValue(), toolSlider.getValue());
 				break;
 				case "Eraser":
-					graphicsContext.clearRect(event.getX(), event.getY(), toolSlider.getValue(), toolSlider.getValue());
+					//graphicsContext.clearRect((int) event.getX(), (int) event.getY(), toolSlider.getValue(), toolSlider.getValue());
+					graphicsContext.setFill(Color.WHITE);
+					graphicsContext.fillRect((int) event.getX(), (int) event.getY(), toolSlider.getValue(), toolSlider.getValue());
 				break;
-			}
-	    });
-		
-		canvas.setOnScroll(event -> {
-			if (event.isControlDown()) {
-				double delta = event.getDeltaY() > 0 ? 0.1 : -0.1;
-				canvas.setScaleX(canvas.getScaleX() + delta);
-				canvas.setScaleY(canvas.getScaleY() + delta);
 			}
 	    });
 		
 		return canvas;
 	}
+	
+	private double clamp(double value, double min, double max) {
+		return Math.max(min, Math.min(value, max));
+	}
 }
+
+
+
+
+
+
+
+
+
