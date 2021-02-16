d637fa4acb3275ab807700c53fe38b6cd1c88a76
magnetenstad <tenstad.magne@gmail.com>
2021-02-16 15:17:58 +0100

    Add PixelApp.java and PixelGUI.fxml

---
+++ project/src/main/java/pixel/PixelApp.java
@@ -0,0 +1,19 @@
+package pixel;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class PixelApp extends Application {
+	@Override
+	public void start(final Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Pixel");
+		primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("PixelGUI.fxml"))));
+		primaryStage.show();
+	}
+
+	public static void main(final String[] args) {
+		Application.launch(args);
+	}
+}
--- project/src/main/java/project/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package project;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
-}
